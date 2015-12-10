2b5d4bd8b155554b47ea6301068720307ac97650
Bigun <a.begun@live.com>
2015-12-10 12:58:06 +0200

    tiket 3: Fixing bugs static files.

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,19 +1,17 @@
 from django.conf.urls import patterns, include, url
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 
 from .settings import DEBUG, MEDIA_ROOT
+
 admin.autodiscover()
 
 urlpatterns = patterns('apps.hello.views',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^$', 'contact_data', name='contact'),
                        url(r'^requests/$', 'requests', name='requests'),
+                       url(r'^uploads/(?P<path>.*)$',
+                           'django.views.static.serve',
+                           {'document_root': MEDIA_ROOT})
                        )
-
-if DEBUG:
-    urlpatterns += patterns('',
-                            url(r'^uploads/(?P<path>.*)$',
-                                'django.views.static.serve',
-                                {'document_root': MEDIA_ROOT})
-                            )
+urlpatterns += staticfiles_urlpatterns()
