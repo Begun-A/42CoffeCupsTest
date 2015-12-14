6e90a4a3e3ccf4c8ace9f2f298c29bdab171632f
Bigun <a.begun@live.com>
2015-12-14 18:34:53 +0200

    ticket 13: Signal chached.

--- apps/hello/signals.py
+++ apps/hello/signals.py
@@ -7,16 +7,17 @@ from models import ObjectsDBLog
 @receiver(post_save)
 def log_objects_updated_added_event(sender, created, **kwargs):
 
-    if created and sender.__name__ != 'ObjectsDBLog':
-        ObjectsDBLog.objects.create(model=sender.__name__, action='created')
+    if sender.__name__ == 'ObjectsDBLog':
+        return
 
-    if not created and sender.__name__ != 'ObjectsDBLog':
-        ObjectsDBLog.objects.create(model=sender.__name__, action='updated')
+    action = 'created' if created else 'updated'
+    ObjectsDBLog.objects.create(model=sender.__name__, action=action)
 
 
 @receiver(post_delete)
 def log_objects_delete_event(sender, **kwargs):
 
-    if sender.__name__ != 'ObjectsDBLog':
-        ObjectsDBLog.objects.create(model=sender.__name__, action='deleted')
-    return
+    if sender.__name__ == 'ObjectsDBLog':
+        return
+
+    ObjectsDBLog.objects.create(model=sender.__name__, action='deleted')
